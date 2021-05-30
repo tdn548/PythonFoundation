e5b277a62db1c549a500424b392238a57ed298ff
tdn548 <53711944+tdn548@users.noreply.github.com>
2021-05-30 17:19:03 -0400

    Update Bai27_Tr7.py

--- Bai27_Tr7.py
+++ Bai27_Tr7.py
@@ -2,14 +2,14 @@ print('Bài 27')
 n=int(input("Nhập n:")) #  n>m >0
 m=int(input("Nhập m:"))
 i=1
-dem=0
+count=0
 
 while i<n:
     if n%i==0 and i<m:
-        if dem==0:
+        if count==0:
             print('Các ước số của %d nhỏ hơn %d: ' % (n, m), end=' ')
         print("%5d" %i,end="")
-        dem=dem+1
-    i=i+1
-if dem==0:
+        count++
+    i++
+if count==0:
     print("Không có ước số nào của %d nhỏ hơn %d" %(n,m))
